188da1eb019a057a25620e2a0d9e16092e1ed636
nakamura0907 <nakamura990907@gmail.com>
2023-05-15 21:38:29 +0900

    現時点では不要なコンポーネントの削除

--- src/components/fade/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Fade: React.FC = () => {
-  return <div></div>;
-};
-
-export default Fade;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// export { default as Fade } from "./components/fade";
-
 export type * from "./components/in-view";
 export { default as InView } from "./components/in-view";
 
